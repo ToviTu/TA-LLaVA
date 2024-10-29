d3c8b4c43dab43899885ead03125143d146fc004
tovitu <tovitusaikou@gmail.com>
2024-10-29 00:54:03 -0500

    implemented the forward method

---
+++ llava/model/language_model/tallava_gemma.py
@@ -0,0 +1,278 @@
+from email.mime import image
+from typing import List, Optional, Tuple, Union
+
+import torch
+import torch.nn as nn
+
+from transformers import AutoConfig, AutoModelForCausalLM, AutoModel
+from transformers.modeling_outputs import CausalLMOutputWithPast
+from transformers.models.gemma2 import Gemma2Config, Gemma2ForCausalLM, Gemma2Model
+
+from ..tallava_arch import TALlavaMetaModel, TALlavaMetaForCausalLM
+from ..multimodal_projector.builder import BimodalIterAttn
+
+from transformers.cache_utils import Cache, HybridCache
+from transformers.modeling_outputs import (
+    BaseModelOutputWithPast,
+    CausalLMOutputWithPast,
+)
+
+
+class TALlavaConfig(Gemma2Config):
+    model_type = "tallava_gemma"
+
+
+class TALlavaGemmaModel(TALlavaMetaModel, Gemma2Model):
+    config_class = TALlavaConfig
+
+    def __init__(self, config: Gemma2Config):
+        super(TALlavaGemmaModel, self).__init__(config)
+
+
+class TALlavaGemmaForCausalLM(Gemma2ForCausalLM, TALlavaMetaForCausalLM):
+    config_class = TALlavaConfig
+
+    def __init__(self, config):
+        super(Gemma2ForCausalLM, self).__init__(config)
+        self.model = TALlavaGemmaModel(config)
+        self.pretraining_tp = 1
+        self.vocab_size = config.vocab_size
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def get_model(self):
+        return self.model
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        seqlens_in_batch: Optional[torch.LongTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        images: Optional[torch.FloatTensor] = None,
+        return_dict: Optional[bool] = None,
+        num_logits_to_keep: int = 0,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+
+        output_attentions = (
+            output_attentions
+            if output_attentions is not None
+            else self.config.output_attentions
+        )
+        output_hidden_states = (
+            output_hidden_states
+            if output_hidden_states is not None
+            else self.config.output_hidden_states
+        )
+        return_dict = (
+            return_dict if return_dict is not None else self.config.use_return_dict
+        )
+
+        if self.get_model().gradient_checkpointing and self.training and use_cache:
+            use_cache = False
+
+        assert inputs_embeds is None, "inputs_embeds not supported"
+
+        # Adjust attention mask length
+        attention_mask_ = torch.ones(
+            input_ids.shape[0],
+            self.config.num_learnable_tokens + input_ids.shape[1],
+        ).to(input_ids.device)
+        if attention_mask is not None:
+            for i, mask_ in enumerate(attention_mask_):  # Copy the mask
+                mask_[self.config.num_learnable_tokens :] = attention_mask[i]
+        attention_mask = attention_mask_
+
+        # Forward pass
+
+        vis_priori = (
+            self.get_model()
+            .vision_priori(
+                torch.arange(self.config.num_learnable_tokens).to(input_ids.device)
+            )
+            .unsqueeze(0)
+            .repeat(input_ids.size(0), 1, 1)
+        )
+        image_embeds = self.encode_images(images)
+        text_embeds = self.get_model().embed_tokens(input_ids)
+
+        inputs_embeds = torch.cat((vis_priori, text_embeds), dim=1)
+
+        # Cache: taken from the original gemma2 implementation
+        if use_cache and past_key_values is None and not self.training:
+            batch_size, seq_len, _ = inputs_embeds.shape
+            past_key_values = HybridCache(
+                self.config,
+                max_batch_size=batch_size,
+                batch_size=batch_size,
+                max_cache_len=seq_len,
+                device=self.device,
+                dtype=inputs_embeds.dtype,
+            )
+
+        if cache_position is None:
+            past_seen_tokens = (
+                past_key_values.get_seq_length() if past_key_values is not None else 0
+            )
+            cache_position = torch.arange(
+                past_seen_tokens,
+                past_seen_tokens + inputs_embeds.shape[1],
+                device=inputs_embeds.device,
+            )
+
+        if position_ids is None:
+            position_ids = cache_position.unsqueeze(0)
+
+        causal_mask = self.get_model()._update_causal_mask(
+            attention_mask,
+            inputs_embeds,
+            cache_position,
+            past_key_values,
+            output_attentions,
+        )
+
+        hidden_states = inputs_embeds
+
+        normalizer = torch.tensor(
+            self.config.hidden_size**0.5, dtype=hidden_states.dtype
+        )
+        hidden_states = hidden_states * normalizer
+
+        # Decoding
+
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attns = () if output_attentions else None
+
+        for i, decoder_layer in enumerate(self.get_model().layers):
+            if output_hidden_states:
+                all_hidden_states = all_hidden_states + (hidden_states,)
+
+            layer_outputs = decoder_layer(
+                hidden_states,
+                attention_mask=causal_mask,
+                position_ids=position_ids,
+                past_key_value=past_key_values,
+                output_attentions=output_attentions,
+            )
+
+            hidden_states = layer_outputs[0]
+
+            if output_attentions:
+                all_self_attns += (layer_outputs[1],)
+
+            # Visual feature pooling
+            if i < len(self.get_model().layers) - 1:
+                mem_embeds = hidden_states[:, : self.config.num_learnable_tokens]
+                text_embeds = hidden_states[:, self.config.num_learnable_tokens :]
+
+                if i % 2 == 0:
+                    mem_embeds = self.get_model().bottle_neck.text_forward(
+                        mem_embeds, text_embeds
+                    )
+                if i % 2 == 1:
+                    mem_embeds = self.get_model().bottle_neck.vis_forward(
+                        mem_embeds, image_embeds
+                    )
+
+                hidden_states = torch.cat((mem_embeds, text_embeds), dim=1)
+
+        hidden_states = self.get_model().norm(hidden_states)
+
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
+        next_cache = past_key_values if use_cache else None
+
+        outputs = tuple(
+            v
+            for v in [hidden_states, next_cache, all_hidden_states, all_self_attns]
+            if v is not None
+        )
+
+        logits = self.lm_head(hidden_states[:, -num_logits_to_keep:, :])
+
+        if self.config.final_logit_softcapping is not None:
+            logits = logits / self.config.final_logit_softcapping
+            logits = torch.tanh(logits)
+            logits = logits * self.config.final_logit_softcapping
+
+        loss = None
+        if labels is not None:
+            loss = self.loss_function(logits, labels, self.vocab_size)
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=past_key_values,
+            hidden_states=hidden_states,
+            attentions=all_self_attns,
+        )
+
+    @torch.no_grad()
+    def generate(
+        self,
+        inputs: Optional[torch.Tensor] = None,
+        images: Optional[torch.Tensor] = None,
+        image_sizes: Optional[torch.Tensor] = None,
+        **kwargs,
+    ) -> Union[GenerateOutput, torch.LongTensor]:
+        position_ids = kwargs.pop("position_ids", None)
+        attention_mask = kwargs.pop("attention_mask", None)
+        if "inputs_embeds" in kwargs:
+            raise NotImplementedError("`inputs_embeds` is not supported")
+
+        if images is not None:
+            (inputs, position_ids, attention_mask, _, inputs_embeds, _) = (
+                self.prepare_inputs_labels_for_multimodal(
+                    inputs,
+                    position_ids,
+                    attention_mask,
+                    None,
+                    None,
+                    images,
+                    image_sizes=image_sizes,
+                )
+            )
+        else:
+            inputs_embeds = self.get_model().embed_tokens(inputs)
+
+        return super().generate(
+            position_ids=position_ids,
+            attention_mask=attention_mask,
+            inputs_embeds=inputs_embeds,
+            **kwargs,
+        )
+
+    def prepare_inputs_for_generation(
+        self, input_ids, past_key_values=None, inputs_embeds=None, **kwargs
+    ):
+        images = kwargs.pop("images", None)
+        image_sizes = kwargs.pop("image_sizes", None)
+        inputs = super().prepare_inputs_for_generation(
+            input_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            **kwargs,
+        )
+        if images is not None:
+            inputs["images"] = images
+        if image_sizes is not None:
+            inputs["image_sizes"] = image_sizes
+        return inputs
+
+
+AutoConfig.register("tallava_gemma", TALlavaConfig)
+AutoModelForCausalLM.register(TALlavaConfig, TALlavaGemmaForCausalLM)
--- llava/model/multimodal_projector/builder.py
+++ llava/model/multimodal_projector/builder.py
@@ -3,6 +3,32 @@ import torch.nn as nn
 import re
 
 
+class CrossAttention(nn.Module):
+    def __init__(self, d_model, d_ffn, num_heads):
+        super().__init__()
+
+        self.pre_attn_norm = nn.LayerNorm(d_model)
+        self.post_attn_norm = nn.LayerNorm(d_model)
+        self.attn = nn.MultiheadAttention(d_model, num_heads, batch_first=True)
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, d_ffn), nn.GELU(), nn.Linear(d_ffn, d_model)
+        )
+
+    def forward(self, x, y, mask=None):
+        query_norm = self.pre_attn_norm(x)
+        key_norm = self.pre_attn_norm(y)
+        value_norm = self.pre_attn_norm(y)
+
+        attn_out, _ = self.attn(query_norm, key_norm, value_norm, attn_mask=mask)
+        attn_out = x + attn_out
+        attn_out = self.post_attn_norm(attn_out)
+
+        ff_out = self.ffn(attn_out)
+        ff_out = attn_out + ff_out
+
+        return ff_out
+
+
 class IdentityMap(nn.Module):
     def __init__(self):
         super().__init__()
@@ -12,7 +38,7 @@ class IdentityMap(nn.Module):
 
     @property
     def config(self):
-        return {"mm_projector_type": 'identity'}
+        return {"mm_projector_type": "identity"}
 
 
 class SimpleResBlock(nn.Module):
@@ -21,22 +47,41 @@ class SimpleResBlock(nn.Module):
         self.pre_norm = nn.LayerNorm(channels)
 
         self.proj = nn.Sequential(
-            nn.Linear(channels, channels),
-            nn.GELU(),
-            nn.Linear(channels, channels)
+            nn.Linear(channels, channels), nn.GELU(), nn.Linear(channels, channels)
         )
+
     def forward(self, x):
         x = self.pre_norm(x)
         return x + self.proj(x)
 
 
-def build_vision_projector(config, delay_load=False, **kwargs):
-    projector_type = getattr(config, 'mm_projector_type', 'linear')
+class BimodalIterAttn(nn.Module):
+    def __init__(self, d_vis, d_model, d_ffn, num_heads):
+        super().__init__()
 
-    if projector_type == 'linear':
+        self.text_xattn = CrossAttention(d_model, d_ffn, num_heads)
+
+        self.vis_xattn = CrossAttention(d_model, d_ffn, num_heads)
+
+    def vis_forward(self, x, y, mask=None):
+        # x should be learned tokens
+        return self.vis_xattn(x, y, mask)
+
+    def text_forward(self, x, y, mask=None):
+        # x should be learned tokens
+        return self.text_xattn(x, y, mask)
+
+    def forward(self, x, y, mask=None):
+        raise NotImplementedError("Use vis_forward or text_forward")
+
+
+def build_vision_projector(config, delay_load=False, **kwargs):
+    projector_type = getattr(config, "mm_projector_type", "linear")
+
+    if projector_type == "linear":
         return nn.Linear(config.mm_hidden_size, config.hidden_size)
 
-    mlp_gelu_match = re.match(r'^mlp(\d+)x_gelu$', projector_type)
+    mlp_gelu_match = re.match(r"^mlp(\d+)x_gelu$", projector_type)
     if mlp_gelu_match:
         mlp_depth = int(mlp_gelu_match.group(1))
         modules = [nn.Linear(config.mm_hidden_size, config.hidden_size)]
@@ -45,7 +90,11 @@ def build_vision_projector(config, delay_load=False, **kwargs):
             modules.append(nn.Linear(config.hidden_size, config.hidden_size))
         return nn.Sequential(*modules)
 
-    if projector_type == 'identity':
+    if projector_type == "identity":
         return IdentityMap()
 
-    raise ValueError(f'Unknown projector type: {projector_type}')
+    # if projector_type == "bimodaliterattn":
+    #     # Hardcoded for now
+    #     return BimodalIterAttn(config.mm_hidden_size, config.hidden_size, config.hidden_size * 4, 8)
+
+    raise ValueError(f"Unknown projector type: {projector_type}")
